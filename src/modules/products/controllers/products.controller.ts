012d3d9c4600e9c30e8ce56e59ab6625c4d57676
Josué Espinoza <142639857+josueespinoza2004@users.noreply.github.com>
2025-05-07 09:36:48 -0600

    customize-continue

--- src/modules/products/controllers/products.controller.ts
+++ src/modules/products/controllers/products.controller.ts
@@ -9,7 +9,11 @@ import {
   Query,
 } from '@nestjs/common';
 import { ProductsService } from '../services/products.service';
-import { CreateProductDto, UpdateProductDto } from '../dto/product.dto';
+import {
+  CreateProductDto,
+  FilterProductDto,
+  UpdateProductDto,
+} from '../dto/product.dto';
 import { PaginationDto } from '../../../common/dto/pagination.dto';
 import { Auth, GetUser } from '../../../auth/decorators';
 import { ValidRoles } from '../../../auth/interfaces';
@@ -20,37 +24,62 @@ export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Get()
-  getFindAll(@Query() paginationDto: PaginationDto) {
+  async getFindAll(@Query() params: FilterProductDto) {
     // console.log(paginationDto);
-    return this.productsService.findAll(paginationDto);
+    const rows = await this.productsService.findAll(params);
+
+    const data = {
+      data: rows,
+    };
+    return data;
   }
 
   @Post()
   @Auth(ValidRoles.admin)
-  createProduct(
+  async create(
     @Body() createProductDto: CreateProductDto,
     @GetUser() user: User,
   ) {
-    return this.productsService.create(createProductDto, user);
+    const nuevo = await this.productsService.create(createProductDto, user);
+    const data = {
+      data: nuevo,
+      message: 'Producto creado correctamente',
+    };
+    return data;
   }
+
   @Get(':id')
-  findOne(@Param('id') id: number) {
-    return this.productsService.findOne(id);
+  async getOne(@Param('id') id: number) {
+    const rows = await this.productsService.findOne(id);
+    const data = {
+      data: rows,
+    };
+    return data;
   }
 
   @Put(':id')
   @Auth(ValidRoles.admin)
-  update(
-    @Param(':id') id: number,
+  async update(
+    @Param('id') id: number,
     @Body() updateProductDto: UpdateProductDto,
     @GetUser() user: User,
   ) {
-    return this.productsService.update(id, updateProductDto, user);
+    const datos = await this.productsService.update(id, updateProductDto, user);
+    const data = {
+      data: datos,
+      message: 'Producto actualizado correctamente',
+    };
+    return data;
   }
 
   @Delete(':id')
   @Auth(ValidRoles.admin)
-  remove(@Param('id') id: number) {
-    return this.productsService.remove(id);
+  async remove(@Param('id') id: number) {
+    const dato = await this.productsService.remove(id);
+    const data = {
+      data: dato,
+      message: 'Producto eliminado correctamente',
+    };
+    return data;
   }
 }
